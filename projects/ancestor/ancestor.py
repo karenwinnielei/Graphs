e8d87f716508d6d49355f86cd1590f6112620224
Karen Lei <karenwinnielei@gmail.com>
2020-09-09 22:06:34 -0700

    complete day 2

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,25 @@
+from util import Stack, Queue
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    queue = Queue()
+    cur = starting_node
+    parent_child = {}
+
+    for node in ancestors:
+        if node[1] not in parent_child:
+            parent_child[node[1]] = set()
+        parent_child[node[1]].add(node[0])
+    
+    if starting_node in parent_child:
+        queue.enqueue(parent_child[cur])
+    else:
+        return -1
+    
+    while queue.size() > 0:
+        connection = queue.dequeue()
+        cur = min(connection)
+        if cur not in parent_child:
+            return cur
+        else:
+            queue.enqueue(parent_child[cur])
+    
